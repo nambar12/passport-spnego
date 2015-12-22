1577c4529bbe9a2d4ac690658e3dbba49fdf1fea
Kushniriov, Michael <michael.kushniriov@intel.com>
2015-12-22 13:35:21 +0200

    steps of 401 in example
    define 401 in example to request token from user

--- example/server.js
+++ example/server.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var app = express();
 var passport = require('passport');
-var KerberosStrategy = require('passport-spnego').Strategy;
+var KerberosStrategy = require('../lib/index').Strategy;
 
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
-    console.log('response:', res);
-    console.log('request:', req);
+app.get('/a', function (req, res) {
+    res.send('Hello World - a');
+//    console.log('response:', res);
+    console.log('request:', req.headers);
 });
+
+app.get('/login', function (req, res) {
+    res.send('Hello World - login');
+//    console.log('response:', res);
+    console.log('request:', req.headers);
+});
+
+
+var success = function () {
+    console.log('Success');
+}
 
 app.post('/login', passport.authenticate('kerberos',
     {
-        successRedirect: '/',
-        failureRedirect: '/login'
+        successRedirect: success,
+        failureRedirect: '/loginfail'
     })
 );
+
+app.get('/loginfail', function(req, res){
+    res.json(401, {message: 'Invalid login details'});
+});
 
 passport.serializeUser(function(user, done) {
     done(null, user.id);
@@ -28,12 +43,16 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new KerberosStrategy({
-        kerberosToken: ''
+        spn: 'SPN',
+        keytab: 'KEYTAB'
     },
     function(token, done) {
-        // ...
+        console.log('new KerberosStrategy, token:' + token);
+        console.log('new KerberosStrategy, done:' + done);
     }
 ));
 
+//passport.authenticate('kerberos');
 
+console.log('listening on port 3000');
 app.listen(3000);
