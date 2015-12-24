f9c137f4d8eee2cbe508e97d90cb36422b55b957
Noam Ambar <noam.ambar@intel.com>
2015-12-24 14:13:21 +0200

    fixed strategy to work with exec file

--- example/server.js
+++ example/server.js
@@ -1,58 +1,39 @@
 var express = require('express');
 var app = express();
 var passport = require('passport');
+var session = require('express-session');
 var KerberosStrategy = require('../lib/index').Strategy;
+var cookieParser = require('cookie-parser');
 
-
-app.get('/a', function (req, res) {
-    res.send('Hello World - a');
-//    console.log('response:', res);
-    console.log('request:', req.headers);
-});
-
-app.get('/login', function (req, res) {
-    res.send('Hello World - login');
-//    console.log('response:', res);
-    console.log('request:', req.headers);
-});
-
-
-var success = function () {
-    console.log('Success');
-}
-
-app.post('/login', passport.authenticate('kerberos',
-    {
-        successRedirect: success,
-        failureRedirect: '/loginfail'
-    })
-);
-
-app.get('/loginfail', function(req, res){
-    res.json(401, {message: 'Invalid login details'});
-});
-
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-    User.findById(id, function(err, user) {
-        done(err, user);
-    });
-});
+app.use(express.static('public'));
+app.use(cookieParser());
+app.use(session({
+    secret: 'kerberos-secret',
+    resave: false,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use(new KerberosStrategy({
         spn: 'SPN',
         keytab: 'KEYTAB'
-    },
-    function(token, done) {
-        console.log('new KerberosStrategy, token:' + token);
-        console.log('new KerberosStrategy, done:' + done);
     }
 ));
 
-//passport.authenticate('kerberos');
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+app.get('/', passport.authenticate('kerberos', {}),
+    function (req, res) {
+        res.send('This is the main page! (user is ' + req.user + ')');
+    });
+
 
 console.log('listening on port 3000');
 app.listen(3000);
